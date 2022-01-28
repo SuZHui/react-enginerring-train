88ab0e7e92b372ceed1877fb52bc85286e701b86
suzh <szh362680581@gmail.com>
2022-01-28 18:03:02 +0800

    update

--- src/pages/Popular/Content.jsx
+++ src/pages/Popular/Content.jsx
@@ -6,13 +6,11 @@ import Card from "./Card";
 export default function Content({ list }) {
   return (
     <Row className="flex items-center justify-around pv3" wrap>
-      {list.map((li, i) => {
-        return (
-          <Col key={li.id + li.name + i} lg={6} md={8} sm={12} xs={24}>
-            <Card {...li} index={i + 1} />
-          </Col>
-        );
-      })}
+      {list.map((li, i) => (
+        <Col key={li.id + li.name + i} lg={6} md={8} sm={12} xs={24}>
+          <Card {...li} index={i + 1} />
+        </Col>
+      ))}
     </Row>
   );
 }
