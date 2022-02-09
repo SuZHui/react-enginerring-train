a08fc5e176fccf133d4a5aecc259dc877d72563e
suzh <szh362680581@gmail.com>
2022-02-09 13:09:48 +0800

    update

--- src/pages/Popular/usePopular.js
+++ src/pages/Popular/usePopular.js
@@ -3,9 +3,13 @@ import API from "@/api";
 
 export default function usePopular(type) {
   const list = useRef([]);
-  const { data, error, size, setSize, mutate } = API.getList(type);
+  const { data, error, size, setSize, mutate, isValidating } =
+    API.getList(type);
 
-  const loadMore = () => setSize(size + 1);
+  const loadMore = () => {
+    if (isValidating) return;
+    setSize(size + 1);
+  };
   list.current = data
     ? data.reduce((a, c = {}) => {
         const { items = [] } = c;
