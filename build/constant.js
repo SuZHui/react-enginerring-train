5e423fa174bc9b4fb241117f524f7b09170ce528
suzh <szh362680581@gmail.com>
2022-02-09 22:59:02 +0800

    Updates

--- build/constant.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // 项目运行地址
-  PROJECT_URL_PATH: "/react-enginerring-train",
-};
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <div className="pt3 pb3 tc bg-light-gray">
-      <span>suzhui©版权所有</span>
-    </div>
-  );
-}
